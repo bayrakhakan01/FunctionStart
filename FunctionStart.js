ead1194213648c00ae46e834680be6a9da59241c
bayrakhakan01 <76584714+bayrakhakan01@users.noreply.github.com>
2021-01-07 23:04:07 +0100

    cleaned "bas function"

--- FunctionStart.js
+++ FunctionStart.js
@@ -55,26 +55,3 @@ btn.onclick = displayMessage();
 */
 
 // btn.addEventListener('click', displayMessage);  // 2.way of calling function
-
-
-function bas() {
-    let searchName = input.value.toLowerCase();
-    /*
-     We store the value entered into the text input in a variable called searchName, 
-     before then emptying the text input and focusing it again, ready for the next search.  
-     */
-    input.value = '';
-    input.focus();
-    for (let i = 0; i < contact.length; i++) {
-        let splitContact = contact[i].split(':');
-        if (splitContact[0].toLowerCase() === searchName) {
-            para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}. ID number is ${i+1}`;
-            console.log(i);
-            break;
-        } else {
-            para.textContent = 'Contact not found.'
-        }
-        
-    }
-   
-}
