51815904c23cf00ad277a00d06172a5f2606d1b1
Hakan Bayrak <bayrakhakan01@gmail.com>
2021-01-07 22:39:26 +0100

    promptand icons

--- FunctionStart.js
+++ FunctionStart.js
@@ -1,4 +1,5 @@
-function displayMessage() {
+function displayMessage(msgText, msgType) {
+  
     const html = document.querySelector('html');
     const panel = document.createElement('div');
 
@@ -6,7 +7,7 @@ function displayMessage() {
     html.appendChild(panel);
 
     const msg = document.createElement('p');
-    msg.textContent = 'This is a massge box';
+    msg.textContent = msgText;
     panel.appendChild(msg);
 
     const closeBtn = document.createElement('button');
@@ -15,10 +16,42 @@ function displayMessage() {
 
     closeBtn.onclick = function () {
         panel.parentNode.removeChild(panel)
+       
+    }
+    if (msgType === 'warning') {
+        msg.style.backgroundImage = 'url(warning.png)';
+        panel.style.backgroundColor = 'red';
+    } else if (msgType === 'chat') {
+        msg.style.backgroundImage = 'url(chat.png)';
+        panel.style.backgroundColor = 'aqua';
+    } else {
+        msg.style.paddingLeft = '20px';
     }
 
+
 }
+function msgpro1() {
+   return prompt('leave a msg :')
+}  
+
+function msgpro2() {
+   let a = prompt('warning or Chat :')
+   return a.toLowerCase().toString();
+ }  
 
 const btn = document.querySelector('button');
+btn.onclick =  function () {
+    displayMessage(msgpro1(),msgpro2());
+} 
+/*
+You might be wondering why we haven't included the parentheses after the function name. 
+This is because we don't want to call the function immediately — only after the button has been clicked. 
+If you try changing the line to and saving and reloading, you'll see that the message box appears without the button being clicked! 
+The parentheses in this context are sometimes called the "function invocation operator". 
+You only use them when you want to run the function immediately in the current scope. 
+In the same respect, the code inside the anonymous function is not run immediately, as it is inside the function scope.
 
-btn.addEventListener('click', displayMessage);
+btn.onclick = displayMessage();
+*/
+
+// btn.addEventListener('click', displayMessage);  // 2.way of calling function
